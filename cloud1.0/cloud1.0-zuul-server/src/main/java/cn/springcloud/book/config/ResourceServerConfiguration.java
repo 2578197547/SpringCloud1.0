0cfe1344691f51cc2267689521b3cd4c7e4ba4bf
xielijie <764682881@qq.com>
2019-12-13 17:25:39 +0800

    zuul error

--- cloud1.0/cloud1.0-zuul-server/src/main/java/cn/springcloud/book/config/ResourceServerConfiguration.java
+++ cloud1.0/cloud1.0-zuul-server/src/main/java/cn/springcloud/book/config/ResourceServerConfiguration.java
@@ -3,6 +3,7 @@ package cn.springcloud.book.config;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
